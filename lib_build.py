53ae686ebb6ecc503009b48831ba0af36e01adc7
jpedley <james.Bennion-Pedley@dyson.com>
2025-01-04 09:15:03 +0000

    Bump and lint

--- lib_build.py
+++ lib_build.py
@@ -53,7 +53,6 @@ devices = {
     "LM4F": "ARM_CM4F",
     "MSP432E4": "ARM_CM4F",
     "SWM050": "ARM_CM0",
-
     # Arduino/STM32Cube macros
     "STM32F0xx": "ARM_CM0",
     "STM32F1xx": "ARM_CM3",
@@ -69,26 +68,29 @@ devices = {
     "STM32L4xx": "ARM_CM4F",
     "STM32L5xx": "ARM_CM33/non_secure",
     "STM32WBxx": "ARM_CM4F",
-    "STM32WLxx": "ARM_CM4F"
+    "STM32WLxx": "ARM_CM4F",
 }
 
 ################################## CONSTANTS ###################################
 
 KERNEL_URL = "https://github.com/FreeRTOS/FreeRTOS-Kernel.git"
-KERNEL_PATH = os.path.realpath('./FreeRTOS-Kernel')
+KERNEL_PATH = os.path.realpath("./FreeRTOS-Kernel")
 
 ############################### HELPER FUNCTIONS ###############################
 
+
 def getRemoteTags(url):
     g = git.cmd.Git()
-    blob = g.ls_remote(url, sort='-v:refname', tags=True, refs=True)
-    entries = blob.split('\n')
-    return list(map(lambda x: x.partition('refs/tags/')[2], entries))
+    blob = g.ls_remote(url, sort="-v:refname", tags=True, refs=True)
+    entries = blob.split("\n")
+    return list(map(lambda x: x.partition("refs/tags/")[2], entries))
+
 
 def getLocalTag(path):
     g = git.Repo(path)
     return [str(next((tag for tag in g.tags if tag.commit == g.head.commit), None))]
     pass
+
 
 ################################## MAIN SCRIPT ##################################
 
@@ -102,7 +104,7 @@ for item in env.get("CPPDEFINES", []):
         if item[0] in devices:
             port.append(item[0])
         # Look for special tags while parsing the macros
-        elif item[0] == 'FREERTOS_TAG':
+        elif item[0] == "FREERTOS_TAG":
             freertos_tag = item[1]
     else:
         if item in devices:
@@ -116,16 +118,21 @@ except:
         # If local kernel is available it can be used, but might not be up to date
         tags = getLocalTag(KERNEL_PATH)
         if not freertos_tag:
-            sys.stderr.write("Could not get remote tags - the local kernel (%s) may not be the most up-to-date\n" % tags[0])
+            sys.stderr.write(
+                "Could not get remote tags - the local kernel (%s) may not be the most up-to-date\n"
+                % tags[0]
+            )
     else:
-        sys.stderr.write("Could not get remote tags - check you can access %s:\n" % KERNEL_URL)
+        sys.stderr.write(
+            "Could not get remote tags - check you can access %s:\n" % KERNEL_URL
+        )
         env.Exit(1)
 
 # Get requested tag name
 if not freertos_tag:
     # Checkout latest tag
     for tag in tags:
-        if tag.find("SMP") == -1:    # Do not include SMP tags
+        if tag.find("SMP") == -1:  # Do not include SMP tags
             tag_name = tag
             break
 else:
@@ -134,15 +141,20 @@ else:
         if freertos_tag == tag:
             tag_name = tag
     if not tag_name:
-        sys.stderr.write("PlatformIO-FreeRTOS: Tag '%s' could not be found in local/remote repository!\n" % freertos_tag)
+        sys.stderr.write(
+            "PlatformIO-FreeRTOS: Tag '%s' could not be found in local/remote repository!\n"
+            % freertos_tag
+        )
         env.Exit(1)
 
 # Is local kernel the correct tag?
-if not(os.path.isdir(KERNEL_PATH)):
+if not (os.path.isdir(KERNEL_PATH)):
     try:
         kernel = git.Repo.clone_from(KERNEL_URL, KERNEL_PATH, depth=1, b=tag_name)
     except:
-        sys.stderr.write("Could not clone Kernel - check you can access %s:\n" % KERNEL_URL)
+        sys.stderr.write(
+            "Could not clone Kernel - check you can access %s:\n" % KERNEL_URL
+        )
         env.Exit(1)
 else:
     # Replace current kernel if wrong version
@@ -152,23 +164,37 @@ else:
 
 # Throw exception if no macros match a device name:
 if len(port) == 0:
-    sys.stderr.write("Error: No device defined for FreeRTOS! Please provide platform manually - options below:\n")
+    sys.stderr.write(
+        "Error: No device defined for FreeRTOS! Please provide platform manually - options below:\n"
+    )
     device_keys = devices.keys()
-    s = ' ,'.join(device_keys)
-    sys.stderr.write(str(s) + '\n')
+    s = " ,".join(device_keys)
+    sys.stderr.write(str(s) + "\n")
     env.Exit(1)
 # Else, add the appropriate port folders to the include path and source filter:
 else:
     if len(port) > 1:
-        sys.stderr.write("Warning: Multiple devices defined: %s\n Using %s as default device.\n" % (" ".join(port), port[0]))
+        sys.stderr.write(
+            "Warning: Multiple devices defined: %s\n Using %s as default device.\n"
+            % (" ".join(port), port[0])
+        )
     print("PlatformIO-FreeRTOS: building for", port[0])
     env.Append(CPPPATH=[os.path.realpath("FreeRTOS-Kernel/include")])
-    env.Append(CPPPATH=[os.path.realpath(os.path.join("FreeRTOS-Kernel/portable/GCC/", devices[port[0]]))])
-    env.Replace(SRC_FILTER=[
+    env.Append(
+        CPPPATH=[
+            os.path.realpath(
+                os.path.join("FreeRTOS-Kernel/portable/GCC/", devices[port[0]])
+            )
+        ]
+    )
+    env.Replace(
+        SRC_FILTER=[
             "+<FreeRTOS-Kernel/>",
+            "-<FreeRTOS-Kernel/examples/>",
             "-<FreeRTOS-Kernel/portable/>",
             "+<FreeRTOS-Kernel/portable/MemMang/heap_4.c>",
-            os.path.join("+<FreeRTOS-Kernel/portable/GCC/", devices[port[0]], ">")
-        ])
+            os.path.join("+<FreeRTOS-Kernel/portable/GCC/", devices[port[0]], ">"),
+        ]
+    )
     # FreeRTOSConfig.h is located in the project space, outside of the lib:
     env.Append(CPPPATH=env.get("PROJECT_INCLUDE_DIR", []))
